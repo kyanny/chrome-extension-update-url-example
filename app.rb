c16466c5ebc9778639c49ddb7b73d4f6e3097aff
Kensuke Nagae <kyanny@gmail.com>
2012-11-05 13:51:08 +0900

    return crx file

--- app.rb
+++ app.rb
@@ -5,8 +5,13 @@ get '/:appname/updates.xml' do
 <?xml version='1.0' encoding='UTF-8'?>
 <gupdate xmlns='http://www.google.com/update2/response' protocol='2.0'>
   <app appid='cmemcpocaaejgcdonnbbgcdcnnhjagah'>
-    <updatecheck codebase='http://localhost:9292/extension.crx' version='1.0' />
+    <updatecheck codebase='http://localhost:9292/hello/extension.crx' version='1.0' />
   </app>
 </gupdate>
 XML
+end
+
+get '/:appname/extension.crx' do
+  content_type 'application/x-chrome-extension'
+  open('extension.crx').read
 end
